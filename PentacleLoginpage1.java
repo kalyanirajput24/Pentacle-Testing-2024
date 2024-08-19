b00765ea99f19d54101ecf52377c4da92cc2baf0
kalyanirajput24 <125184091+kalyanirajput24@users.noreply.github.com>
2024-08-19 10:40:48 +0530

    Add files via upload

--- PentacleLoginpage1.java
+++ PentacleLoginpage1.java
@@ -26,17 +26,39 @@ public class PentacleLoginpage1 {
 		driver.get(baseurl);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 	}
+
+	@Test (priority = 1)
+	public void doLoginWithInValidCredentials() throws InterruptedException
+	{
+		//find Email & enter email ("hr@weoto.in")
+		driver.findElement(By.xpath("/html/body/div/div/div/main/div[2]/div/form/div[1]/div/div[1]/div/input")).sendKeys("hr@weoto.in");
+
+		//find Password & enter password ("superAdmin@123")
+		driver.findElement(By.name("password")).sendKeys("superAdmin@124");//Wrong Password
+
+		//click on submit button
+		driver.findElement(By.xpath("/html/body/div/div/div/main/div[2]/div/form/button")).click();
+
+		String message_expected = "Invalid Credentials";
+		String message_actual = driver.findElement(By.xpath("/html/body/div/div[1]/div/span[2]")).getText();
+		//Invalid email or password
+
+		Assert.assertEquals(message_expected, message_actual);
+		Thread.sleep(1500);
+
+	}
+
 	@Test (priority = 2)
 	public void logintestWithValidcredentials() {
 		//find Email & enter email ("hr@weoto.in")
 		driver.findElement(By.xpath("/html/body/div/div/div/main/div[2]/div/form/div[1]/div/div[1]/div/input")).sendKeys("hr@weoto.in");
-		
+
 		//find Password & enter password ("superAdmin@123")
 		driver.findElement(By.name("password")).sendKeys("superAdmin@123");
-		
+
 		//click on submit button
 		driver.findElement(By.xpath("/html/body/div/div/div/main/div[2]/div/form/button")).click();
-		
+
 		//verify if the login was successfully by checking the page title or a specific element 
 		String pageTitle = driver.getTitle();
 
@@ -49,28 +71,22 @@ public class PentacleLoginpage1 {
 
 		Assert.assertEquals("Pentacle ERP", pageTitle);
 	}
-	
-	@Test (priority = 1)
-	public void doLoginWithInValidCredentials() throws InterruptedException
-	{
-		//find Email & enter email ("hr@weoto.in")
-				driver.findElement(By.xpath("/html/body/div/div/div/main/div[2]/div/form/div[1]/div/div[1]/div/input")).sendKeys("hr@weoto.in");
-				
-				//find Password & enter password ("superAdmin@123")
-				driver.findElement(By.name("password")).sendKeys("superAdmin@124");//Wrong Password
-				
-				//click on submit button
-				driver.findElement(By.xpath("/html/body/div/div/div/main/div[2]/div/form/button")).click();
-				
-				String message_expected = "Invalid Credentials";
-				String message_actual = driver.findElement(By.xpath("/html/body/div/div[1]/div/span[2]")).getText();
-				//Invalid email or password
-				
-				Assert.assertEquals(message_expected, message_actual);
-				Thread.sleep(1500);
-			//	/html/body/div/div[1]/div/span[2]
-	}
 
+	@Test (priority=3)
+	public void addEmployee() 
+
+	{   //Click on HR
+		driver.findElement(By.xpath("//h2[contains(text(),'HR')]")).click();
+
+		// Step 7: Click on Employees
+		driver.findElement(By.xpath("/html/body/div[1]/div/section/div[1]/div/div[2]/div[3]/ul/li/ul/li[1]/a/span")).click();
+		//Step 8 :Click on Add Employee Button 
+		driver.findElement(By.xpath("/html/body/div[1]/div/section/div[2]/main/div/div[1]/div/div[2]/button[3]")).click(); 
+
+		//Step 9: Add on Employees Details 
+		driver.findElement(By.name("firstName")).sendKeys("Kalyani");
+		driver.findElement(By.name("lastName")).sendKeys("Patil"); 
+	}
 
 
 	public void logout()
@@ -78,6 +94,8 @@ public class PentacleLoginpage1 {
 		((WebElement) driver.findElements(By.xpath("/html/body/div[1]/div/section/div[2]/div/nav/header/ul[3]/li[1]/button/span"))).click();
 		((WebElement) driver.findElements(By.xpath("//*[@id=\"react-aria1455909104-:r1q:\"]"))).click();
 	}
+
+
 	@AfterTest
 	public void tearDown() throws InterruptedException {
 
